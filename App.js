edeea4b7685b89390086b590f2e0daa55d47b8f8
Daniel Brandt <dbbrandt@gmail.com>
2023-01-25 17:24:58 -0500

    Working Category Grid. Done as pre-work to course demos.

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomePage from "./screens/homePage";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <HomePage>Category Home Page!</HomePage>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,6 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 60,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
