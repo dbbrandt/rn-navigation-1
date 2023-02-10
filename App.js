2c62b63528c346a0a78f7c6d0e8595a67f7b6ab7
Daniel Brandt <dbbrandt@gmail.com>
2023-02-10 15:58:12 -0500

    Added Context and Provider.

--- App.js
+++ App.js
@@ -1,69 +1,77 @@
+import {useState} from 'react';
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, View, } from 'react-native';
-import { NavigationContainer, CommonActions  } from "@react-navigation/native";
+import {StyleSheet, View,} from 'react-native';
+import {NavigationContainer, CommonActions} from "@react-navigation/native";
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import MealsHomeScreen from "./screens/mealsHomeScreen";
+import { MealsContext } from "./components/mealsContext";
 import {Ionicons} from '@expo/vector-icons';
 
 const Drawer = createDrawerNavigator();
-const favoriteMeals = [];
 
 export default function App() {
+    const [favoriteMeals, setFavoriteMeals] = useState([]);
+
     return (
-        <View style={styles.rootContainer}>
-            <StatusBar style="light"/>
-            <NavigationContainer>
-                <Drawer.Navigator
-                    initialRouteName='FavoritesOverview'
-                    conentOptions={{
-                        activeTintColor: 'white',
-                        activeBackgroundColor: 'transparent',
-                        inactiveTintColor: 'black',
-                        inactiveBackgroundColor: 'transparent',
-                    }}
-                    screenOptions={({
-                        headerShown: false,
-                        drawerActiveTintColor: 'white',
-                        drawerInactiveTintColor: '#512203',
-                        drawerStyle: {
-                            backgroundColor: '#b79783',
-                            width: 200,
-                        },
-                })}>
-                    <Drawer.Screen name='Home'
-                                   component={MealsHomeScreen}
-                                   initialParams={{
-                                       initialRoute: 'MealsCategories',
-                                       favoriteMeals: favoriteMeals,
-                                       goHome: false,
-                                   }}
-                                   listeners={({navigation, route}) => ({
-                                       drawerItemPress: (e) =>{
-                                           navigation.dispatch(CommonActions.setParams({ goHome: true }));
-                                       }
-                                   })}
-                                   options={{
-                                       drawerIcon: ({color, size}) =>
-                                           <Ionicons name="home" color={color} size={size}/>
-                                   }}
-                    />
-                    <Drawer.Screen name='FavoritesHome'
-                                   component={MealsHomeScreen}
-                                   initialParams={{
-                                       initialRoute: 'Favorites',
-                                       favoriteMeals: favoriteMeals
-                                   }}
-                                   options={{
-                                       drawerLabel: 'Favorites',
-                                       drawerIcon: ({color, size}) =>
-                                           <Ionicons name="star" color={color} size={size}/>
-                                   }}
+        <MealsContext.Provider value={{
+            favoriteMeals: favoriteMeals,
+            setFav: (meals) => setFavoriteMeals(meals),
+            test: () => console.log('test method')
+        }}>
+            <View style={styles.rootContainer}>
+                <StatusBar style="light"/>
+                <NavigationContainer>
+                    <Drawer.Navigator
+                        initialRouteName='FavoritesOverview'
+                        conentOptions={{
+                            activeTintColor: 'white',
+                            activeBackgroundColor: 'transparent',
+                            inactiveTintColor: 'black',
+                            inactiveBackgroundColor: 'transparent',
+                        }}
+                        screenOptions={({
+                            headerShown: false,
+                            drawerActiveTintColor: 'white',
+                            drawerInactiveTintColor: '#512203',
+                            drawerStyle: {
+                                backgroundColor: '#b79783',
+                                width: 200,
+                            },
+                        })}>
+                        <Drawer.Screen name='Meals'
+                                       component={MealsHomeScreen}
+                                       initialParams={{
+                                           initialRoute: 'MealsCategories',
+                                           goHome: false,
+                                       }}
+                                       listeners={({navigation, route}) => ({
+                                           drawerItemPress: (e) => {
+                                               navigation.dispatch(CommonActions.setParams({goHome: true}));
+                                           }
+                                       })}
+                                       options={{
+                                           drawerIcon: ({color, size}) =>
+                                               <Ionicons name="book" color={color} size={size}/>
+                                       }}
+                        />
+                        <Drawer.Screen name='FavoritesHome'
+                                       component={MealsHomeScreen}
+                                       initialParams={{
+                                           initialRoute: 'Favorites',
+                                       }}
+                                       options={{
+                                           drawerLabel: 'Favorites',
+                                           drawerIcon: ({color, size}) =>
+                                               <Ionicons name="star" color={color} size={size}/>
+                                       }}
 
-                    />
+                        />
 
-                </Drawer.Navigator>
-            </NavigationContainer>
-        </View>
+                    </Drawer.Navigator>
+                </NavigationContainer>
+            </View>
+        </MealsContext.Provider>
+
     );
 }
 
